1441bced680a899f00d32bd8c49e7776288129f6
TalkBog <99033517+TalkBog@users.noreply.github.com>
2023-11-07 09:06:01 +0100

    Tests nextauthjs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,33 @@
+import {
+  LoginButton,
+  LogoutButton,
+  ProfileButton,
+  RegisterButton,
+} from "@/components/buttons.component";
+import { User } from "@/components/user.component";
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+  console.log("session: " + JSON.stringify(session));
   return (
-    <main>
-
+    <main
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "70vh",
+      }}
+    >
+      <div>
+        <LoginButton />
+        <RegisterButton />
+        <LogoutButton />
+        <ProfileButton />
+        <User />
+      </div>
     </main>
-  )
+  );
 }
+

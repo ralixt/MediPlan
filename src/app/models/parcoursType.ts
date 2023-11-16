39522cc33c42a2a4d946372ab45e3b2c9db157b3
batos1er <baptiste.menetrat@iut-dijon.u-bourgogne.fr>
2023-11-16 09:46:35 +0100

    changement nom et correction BDD json

--- src/app/models/parcoursType.ts
+++ src/app/models/parcoursType.ts
@@ -4,7 +4,7 @@ import { EtapeType } from './etapeType';
 interface IParcoursType extends Document {
     name: string;
     type: string;
-    Sequencable: Types.ObjectId[];
+    sequencables: Types.ObjectId[];
     precedences: {
         name: string;
         antecedent: Types.ObjectId;
@@ -15,7 +15,7 @@ interface IParcoursType extends Document {
 const parcoursTypeSchema = new Schema<IParcoursType>({
     name: { type: String, required: true },
     type: { type: String, required: true },
-    Sequencable: [{ type: Schema.Types.ObjectId, ref: 'EtapeType' }],
+    sequencables: [{ type: Schema.Types.ObjectId, ref: 'EtapeType' }],
     precedences: [{
         name: { type: String, required: true },
         antecedent: { type: Schema.Types.ObjectId, ref: 'EtapeType', required: true },

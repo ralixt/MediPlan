3ac96a5f9923e44823062eea2fe3578c562d2b01
batos1er <baptiste.menetrat@iut-dijon.u-bourgogne.fr>
2023-11-28 09:52:11 +0100

    Update not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,14 @@
+import NavBar from '@/components/navBar'
 import Link from 'next/link'
  
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/home">Return Home</Link>
-    </div>
+      <section className="w-full bg-light-blue h-[50vh] ">
+        <div className="ml-5 flex flex-col items-center justify-around h-full">
+          <h2>Not Found</h2>
+          <p>Could not find requested resource</p>
+          <Link href="/home">Return Home</Link>
+        </div>
+      </section>    
   )
 }

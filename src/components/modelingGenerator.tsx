75acd520cebcfb27b5a3071e29bf6ab6a5e6f0e2
TalkBog <99033517+TalkBog@users.noreply.github.com>
2023-12-14 10:18:51 +0100

    Update modelingGenerator.tsx

--- src/components/modelingGenerator.tsx
+++ src/components/modelingGenerator.tsx
@@ -1,34 +1,33 @@
-"use client"
-import {cache, useEffect, useState} from "react";
-import {usePathname} from "next/navigation";
+"use client";
+import { cache, useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
 import Link from "next/link";
-import {Clock, Door, DotsThreeOutlineVertical, ForkKnife} from "@phosphor-icons/react";
-import {DndContext} from "@dnd-kit/core"
-
+import {
+  Clock,
+  Door,
+  DotsThreeOutlineVertical,
+  ForkKnife,
+} from "@phosphor-icons/react";
+import { DndContext } from "@dnd-kit/core";
 
 type props = {
-    element: React.ReactNode[],
-    parcour: parcours
-}
+  element: React.ReactNode[];
+  parcour: parcours;
+};
 
-export default function ModelingGenerator({element, parcour}: props) {
-    const [elements, setElement] = useState(element)
-    return(
-        <div
-            className="flex flex-row gap-56 w-full overflow-x-scroll px-48 h-full items-center"
-            onWheel={(e) => {
-                e.preventDefault();
-                const container = e.currentTarget;
-                const delta = Math.max(-1, Math.min(1, e.deltaY));
-                container.scrollLeft += delta * 100; // Ajustez la valeur pour contrôler la vitesse du défilement horizontal
-              }}
-        >
-            <DndContext>
-                {elements.map((element)=> element)}
-            </DndContext>
-        </div>
-
-
-    )
-
+export default function ModelingGenerator({ element, parcour }: props) {
+  const [elements, setElement] = useState(element);
+  return (
+    <div
+      className="flex flex-row gap-20 w-full overflow-x-scroll pl-48 pr-[50rem] h-full items-center"
+      onWheel={(e) => {
+        e.preventDefault();
+        const container = e.currentTarget;
+        const delta = Math.max(-1, Math.min(1, e.deltaY));
+        container.scrollLeft += delta * 100; // Ajustez la valeur pour contrôler la vitesse du défilement horizontal
+      }}
+    >
+      <DndContext>{elements.map((element) => element)}</DndContext>
+    </div>
+  );
 }

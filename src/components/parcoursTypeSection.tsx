c89e0ba308025936490da5418342e4d043f150be
TalkBog <99033517+TalkBog@users.noreply.github.com>
2024-03-05 13:28:26 +0100

    reglage bugs

--- src/components/parcoursTypeSection.tsx
+++ src/components/parcoursTypeSection.tsx
@@ -13,14 +13,14 @@ import { Parachute } from "@phosphor-icons/react";
 import PlanificationParcours from "./planificationParcours";
 
 type props = {
-  Planification_id:string;
+  Planification_id: string;
   journeeType: JourneeType;
   setMaj: Dispatch<SetStateAction<boolean>>;
   parcours: parcoursList;
 };
 
 export default function ParcoursTypeSection({
-                                              Planification_id,
+  Planification_id,
   journeeType,
   setMaj,
   parcours,
@@ -29,13 +29,12 @@ export default function ParcoursTypeSection({
 
   return (
     <div>
-      <div className="flex items-center content-center justify-center flex-wrap">
+      <div className="flex items-start content-start justify-between flex-wrap gap-10">
         {parcours.map((parcour) => {
           let dataPlanif = journeeType.planificationParcours.find(
             (value) => value.idParcours === parcour._id
           );
           if (!dataPlanif) {
-
             journeeType.planificationParcours.push({
               idParcours: parcour._id,
               nbParcours: 0,
@@ -49,17 +48,15 @@ export default function ParcoursTypeSection({
             console.log(dataPlanif);
           }
 
-
           if (dataPlanif) {
-
-            console.log("cc",dataPlanif);
+            console.log("cc", dataPlanif);
             return (
               <PlanificationParcours
                 key={parcour._id}
                 id={parcour._id}
                 Planification_id={Planification_id}
                 JourneeType_id={journeeType._id}
-                JourneeType = {journeeType}
+                JourneeType={journeeType}
                 name={parcour.name}
                 dataPlanif={dataPlanif}
               />
--- src/components/planificationParcours.tsx
+++ src/components/planificationParcours.tsx
@@ -1,74 +1,73 @@
-import {Minus, Plus} from "@phosphor-icons/react";
-import {useEffect, useState} from "react";
-import {updateNumberParcours} from "@/actions/Planification";
+import { Minus, Plus } from "@phosphor-icons/react";
+import { useEffect, useState } from "react";
+import { updateNumberParcours } from "@/actions/Planification";
 import JourneeType from "@/app/models/journeeType";
 
 type PlanificationParcoursProps = {
   id: string;
-    Planification_id:string;
+  Planification_id: string;
   name: string;
-  JourneeType_id:string;
-    JourneeType:JourneeType;
+  JourneeType_id: string;
+  JourneeType: JourneeType;
   dataPlanif: parcoursJourneeType;
 };
 
 export default function PlanificationParcours({
   id,
-                                                  Planification_id,
+  Planification_id,
   name,
-                                                  JourneeType_id,
-                                                  JourneeType,
+  JourneeType_id,
+  JourneeType,
   dataPlanif,
-
 }: PlanificationParcoursProps) {
+  const [nbParcours, setNbParcours] = useState<number>(dataPlanif.nbParcours);
 
+  const handlePlusClick = () => {
+    setNbParcours((i) => i + 1);
+  };
 
+  const handleMinusClick = () => {
+    if (nbParcours > 0) {
+      setNbParcours((i) => i - 1);
+    }
+  };
 
-    const [nbParcours, setNbParcours] = useState<number>(dataPlanif.nbParcours);
+  useEffect(() => {
+    async function updateParcours() {
+      await updateNumberParcours(
+        Planification_id,
+        JourneeType_id,
+        nbParcours,
+        dataPlanif.idParcours,
+        JourneeType
+      );
+    }
 
-    const handlePlusClick = () => {
-        setNbParcours((i)=>i+1)
-    };
-
-    const handleMinusClick = () => {
-        if (nbParcours > 0) {
-            setNbParcours((i)=>i-1)
-        }
-    };
-
-    useEffect(() => {
-        async function updateParcours() {
-            await updateNumberParcours(Planification_id,JourneeType_id, nbParcours, dataPlanif.idParcours,JourneeType);
-        }
-
-        updateParcours();
-    }, [nbParcours]);
+    updateParcours();
+  }, [nbParcours]);
   return (
-      <div key={id} className="flex flex-row items-center content-center justify-center text-xl my-4 w-1/2">
+    <div key={id} className="w-96 text-xl my-4 ">
+      <div className="flex flex-row mr-10">
         <div className="w-52 h-20 bg-lightlightgrey shadow-md rounded-xl flex items-center content-center justify-center p-4">
           <p>{name}</p>
         </div>
         <div className="w-20 h-20 bg-lightlightgrey ml-12 mr-6 flex items-center shadow-md rounded-xl p-4">
-            <div className="h-full border-negative border-4 rounded-xl">
-            </div>
-            <div className="ml-4">
-                {dataPlanif.pourcentage_utilisation}
-            </div>
+          <div className="h-full border-negative border-4 rounded-xl"></div>
+          <div className="ml-4">{dataPlanif.pourcentage_utilisation}</div>
         </div>
         <div className="w-48 h-20 bg-light-blue rounded-xl ">
-            <div className="w-full h-full bg-lightlightgrey text-4xl flex items-center justify-around shadow-md rounded-xl p-4 translate-x-[-0.3rem] translate-y-[-0.3rem]">
-                <button onClick={handleMinusClick}>
-                    <Minus size={32}/>
-                </button>
+          <div className="w-full h-full bg-lightlightgrey text-4xl flex items-center justify-around shadow-md rounded-xl p-4 translate-x-[-0.3rem] translate-y-[-0.3rem]">
+            <button onClick={handleMinusClick}>
+              <Minus size={32} />
+            </button>
 
-                <div>
-                    {nbParcours}
-                </div>
-                <button onClick={handlePlusClick}>
-                    <Plus size={32}/>
-                </button>
-            </div>
+            <div>{nbParcours}</div>
+            <button onClick={handlePlusClick}>
+              <Plus size={32} />
+            </button>
+          </div>
         </div>
       </div>
+    </div>
   );
 }
